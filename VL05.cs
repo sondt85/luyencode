79b4a8874cd724692d782d0c3a0e40e4f63e887c
sondt85 <40972889+sondt85@users.noreply.github.com>
2022-08-31 14:30:48 +0700

    Add files via upload

---
+++ VL05.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            int n = Convert.ToInt32(s);
+            int sum = 0;
+            for(int i =0; i<=3*n+1; i++)
+            {
+                if (i % 2 == 0) sum = sum - i;
+                if (i % 2 == 1) sum = sum + i;
+            }
+            Console.WriteLine("{0}", sum);
+            Console.ReadLine();
+        }
+    }
+}
