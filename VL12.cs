4e2c325a69ce6cff612f5d58702693d1d84a2211
sondt85 <40972889+sondt85@users.noreply.github.com>
2022-09-07 14:49:09 +0700

    Add files via upload

---
+++ VL12.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VL12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = Console.ReadLine();
+            int n = Convert.ToInt32(s);
+            if (n == 0) Console.Write("INF");
+            else
+            {
+                for (int i = n; i > 0; i--)
+                    if (n % i == 0)
+                        Console.Write("{0} ", i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
